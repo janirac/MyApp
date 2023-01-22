22e7b674fd61f31adbfee22d41b195241aa36a0b
Janira Crispin <janiracrispin@gmail.com>
2023-01-22 18:46:49 -0500

    create route for app and user using browserrouter

--- src/index.js
+++ src/index.js
@@ -2,10 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Users from './components/Users';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Route path="/">
+          <App />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+      </div>
+      {/* <App /> */}
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
